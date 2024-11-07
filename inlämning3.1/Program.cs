fe6079cb10e54dfaee9492898ead115496be8bfd
hulj07001 <hulj07001@E-PW05CGR7>
2024-11-07 12:16:05 +0100

    Add project files.

---
+++ inlämning3.1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace inlämning3_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Hej! Ange din ålder för att få se om du får delta i tävlingen:");
+                int ålder = int.Parse(Console.ReadLine());
+
+                if (ålder < 16)
+                {
+                    Console.WriteLine("Du är för ung för att delta.");
+                }
+                else if (ålder > 19)
+                {
+                    Console.WriteLine("Du är för gammal för att delta.");
+                }
+                else
+                {
+                    Console.WriteLine("Du får delta!");
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Skriv in hela antal.");
+            }
+        }
+    }
+}
